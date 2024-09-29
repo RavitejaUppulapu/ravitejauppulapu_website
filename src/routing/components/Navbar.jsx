58432a0294c6ac347769d365e5f1a7cbe3d6d674
Raviteja Uppulapu <raviravteja2124@gmail.com>
2024-09-29 13:57:12 +0530

    check

--- src/routing/components/Navbar.jsx
+++ src/routing/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-logo">Raviteja Uppulapu</div>
-      <ul className="nav-links">
+      <div className="nav-toggle" onClick={toggleNavbar}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/education">Education</Link></li>
